01d3441bc29360dc3230e4ef84c606a5c3f02d4a
Caleb Aitken <caleb@jasa.id.au>
2019-08-04 16:11:21 +1000

    Added cmake file and simple build functionality

---
+++ cmake-build-debug/CMakeFiles/bark.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bark.dir/bark.c.obj"
+  "bark.pdb"
+  "bark.exe"
+  "bark.exe.manifest"
+  "bark.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
